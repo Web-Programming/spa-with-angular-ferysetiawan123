bc68a368f9718a0934698dc155fa664990990c8e
fery <ferysetiawan_2226240137@mhs.mdp.ac.id>
2025-11-24 16:06:31 +0700

    update
    update

--- griya-mdp/src/app/register/register.spec.ts
+++ griya-mdp/src/app/register/register.spec.ts
@@ -1,4 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  registerForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  
+}
+}
+
 
 import { Register } from './register';
 
