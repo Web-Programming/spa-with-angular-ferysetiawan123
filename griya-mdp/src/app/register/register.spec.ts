fc4a91197b596fd8b691fced61dce13bba0b3aaa
fery <ferysetiawan_2226240137@mhs.mdp.ac.id>
2025-11-24 16:14:57 +0700

    perbaikan register
    perbaikan register

--- griya-mdp/src/app/register/register.spec.ts
+++ griya-mdp/src/app/register/register.spec.ts
@@ -1,46 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  registerForm: FormGroup;
-  
-  constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(2)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
-  });
-  
+
 }
-}
-
-
-import { Register } from './register';
-
-describe('Register', () => {
-  let component: Register;
-  let fixture: ComponentFixture<Register>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Register]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(Register);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
